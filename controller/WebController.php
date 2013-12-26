8f2b6b26bed1d2529f69c45d5e804848d2129513
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-26 19:44:37 +0100

    Introduce Controller to represent the old 'view' approach

---
+++ controller/WebController.php
@@ -0,0 +1,41 @@
+<?
+
+abstract class WebController extends Controller
+{
+
+	public function getUsedModels()
+	{
+		return array();
+	}
+	
+	function css()
+	{
+		$resources = Project::getCurrent()->getResources();		
+		if (is_array($resources['css'])) {
+			return "<!-- CSS -->\n".produce("$ {\t\t[*:css]\n }",$resources['css']);
+		} else {
+			return '<!-- NO css -->';
+		}
+		
+	}
+	
+	function javascript()
+	{
+		$resources = Project::getCurrent()->getResources();
+		if (is_array($resources['js'])) {
+			return "<!-- Javascript -->\n".produce("$ {\t\t[*:javascript]\n }",$resources['js']);
+		} else {
+			return '<!-- NO javascript -->';
+		}
+		
+	}
+	
+	
+	function worktime()
+	{
+		return round(Application::ExecutionTime()*1000,2);
+	}
+	
+}
+
+?>
