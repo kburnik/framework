e2edac3a4eca2e44aaa6ed3bdfc978a3ae8f89f5
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-26 04:39:01 +0100

    FileViewProvider maps view keys to filenames

---
+++ view/FileViewProvider.php
@@ -0,0 +1,48 @@
+<?
+
+
+class FileViewProvider extends ViewProvider 
+{
+	
+	private $map = array();
+	
+	function __construct( $map = array() ) 
+	{
+		$this->map = $map;
+	}
+	
+	function getTemplate( $viewKey ) 
+	{
+		$filename = $this->map[ $viewKey ];
+		
+		// fallback to default view location
+		if ( !file_exists ( $filename ) ) 
+		{
+			$filename = view( $filename );		
+		}
+		
+		if ( ! file_exists( $filename ) ) 
+		{
+			throw new Exception("Missing view file: $filename");
+		}
+		
+		if ($template = file_get_contents( $filename )) 
+		{		
+			return $template;
+		} 
+		else 
+		{
+			throw new Exception("Cannot open view file: $filename");
+		}
+		
+	}
+	
+	function containsTemplate( $viewKey )
+	{
+		return array_key_exists( $viewKey , $this->map );
+	}
+	
+	
+}
+
+?>
