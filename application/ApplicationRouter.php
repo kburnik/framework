59502a4ff231424830f8ec15900d1c79dca95bea
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-27 05:18:48 +0100

    Add redirection support to Application Router when controller exits

--- application/ApplicationRouter.php
+++ application/ApplicationRouter.php
@@ -1,1 +1,138 @@
-<?abstract class ApplicationRouter {		protected $routes;		public abstract function getViewProvider( $controllerClassName );		public function __construct( $routes ) 	{		$this->routes = $routes;	}		public function route( $url , $templateViewFilename , $notFoundViewFilename ) 	{					$controller = $this->getControllerForRoute( $url );							if ( $controller instanceOf Controller )		{			return produceview( $templateViewFilename,  $controller );		} 		else 		{			header('HTTP/1.1 404 Not Found');			return produceview( $notFoundViewFilename , array( "url" => $url ) );		}		}			private function getControllerForRoute( $url )	{			$routes = $this->routes;						$controllerMatched = false;				if ( array_key_exists( $url , $routes ) ) 		{					$controllerMatched = true;						list( $controllerClassName , $controllerParams ) = $routes[ $url ];							} 		else 		{								foreach ( $routes as $pattern => $routeInstructions ) 					{							list( $className , $controllerParams , $matchMapping ) = $routeInstructions;												if ( @preg_match(  "/{$pattern}/"  , $url , $matchResults ) ) 				{									foreach ( $matchMapping as $varName => $index )					{											$controllerParams[ $varName ] = $matchResults[ $index ];										}										/*					print_r( array(						matched => $className,						url => $url,						pattern => $pattern,						matchMapping => $matchMapping,						matchResults => $matchResults,						controllerParams => $controllerParams					));					*/										$controllerClassName = $className;										$controllerMatched = true;										break;									}									}							}								if ( $controllerMatched )		{							$viewProvider = $this->getViewProvider( $controllerClassName );												$controller = 				new $controllerClassName				( 					null					, 					$controllerParams					,					$viewProvider									);						return $controller;						}				return null;				}}?>
+<?
+
+abstract class ApplicationRouter 
+{
+	
+	protected $routes;
+	
+	// default route to take when the controller does not provide one via exitEventParam
+	protected $defaultExitRoute = null;
+	
+	public abstract function getViewProvider( $controllerClassName );
+	
+	
+	// redirect to another route when controller exits
+	public abstract function redirect( $controller );
+	
+	public function __construct( $routes ) 
+	{
+		$this->routes = $routes;
+	}
+	
+	public function route( $url , $templateViewFilename , $notFoundViewFilename ) 
+	{
+	
+		
+		$controller = $this->getControllerForRoute( $url );			
+		
+		if ( $controller instanceOf Controller )
+		{
+		
+			if ( $controller->exited )
+			{
+				$this->redirect( $controller );
+			} 
+			else 
+			{
+				return produceview( $templateViewFilename,  $controller );
+			}
+		} 
+		else 
+		{
+			header('HTTP/1.1 404 Not Found');
+			return produceview( $notFoundViewFilename , array( "url" => $url ) );
+		}
+
+	
+	}
+	
+	
+	private function getControllerForRoute( $url )
+	{
+	
+		$routes = $this->routes;
+		
+		
+		$controllerMatched = false;
+		
+		// direct match
+		if ( array_key_exists( $url , $routes ) ) 
+		{
+		
+			$controllerMatched = true;
+			
+			list( $controllerClassName , $controllerParams , $matchMapping , $defaultExitRoute ) = $routes[ $url ];
+			
+		
+		} 
+		else 
+		{
+		// regex match
+			
+			foreach ( $routes as $pattern => $routeInstructions ) 		
+			{
+			
+				list( $className , $controllerParams , $matchMapping , $defaultExitRoute ) = $routeInstructions;
+				
+				
+				if ( @preg_match(  "/{$pattern}/"  , $url , $matchResults ) ) 
+				{
+					
+					$controllerClassName = $className;
+					
+					$controllerMatched = true;
+					
+					break;
+					
+				}
+						
+			}
+			
+		
+		}
+		
+		
+		
+		if ( $controllerMatched )
+		{
+				
+			$viewProvider = $this->getViewProvider( $controllerClassName );
+
+			$this->defaultExitRoute = $defaultExitRoute;
+			
+			
+			// extend controller params with regex matches
+			if ( is_array( $matchMapping ) )
+			{
+				foreach ( $matchMapping as $varName => $index )
+				{
+				
+					$controllerParams[ $varName ] = $matchResults[ $index ];
+				
+				}
+				
+			}
+				
+			$controller = 
+				new $controllerClassName
+				( 
+					null
+					, 
+					$controllerParams
+					,
+					$viewProvider					
+				);
+			
+			return $controller;
+				
+		}
+		
+		return null;
+		
+	
+	}
+
+}
+
+
+?>
