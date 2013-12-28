30220e669b50700988a9d4d22fa794cc4cddd293
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-28 15:06:07 +0100

    add default routes for XHR entity model

---
+++ responder/entity.model.xhr.routes.php
@@ -0,0 +1,33 @@
+<?
+
+return array(
+
+		
+	'\/(.*)\/([0-9]+)(\?(.*)){0,1}' => 
+		array( 
+			'ApiResponder' , 
+			array( 'action' => 'findById' ) , 
+			array( 'entity' => 1 , 'id' => 2 ) 
+		) ,
+	
+	'\/(.*)\/@([A-Za-z_]+[A-Za-z0-9_]*)(\?(.*)){0,1}' => 
+		array( 
+			'ApiResponder' , 			
+			array(),
+			array( 'entity' => 1 , 'action' => 2 ) 
+		) ,
+	
+	'\/(.*)\/(\?(.*)){0,1}' => 
+		array( 
+			'ApiResponder' , 
+			array( 'action' => 'find' ) , 
+			array( 'entity' => 1 ) 
+		) ,
+		
+	
+	
+
+);
+
+
+?>
