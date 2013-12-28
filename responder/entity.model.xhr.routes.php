cc661f8314775460f19f1734796a73cecbae9166
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-28 15:07:06 +0100

    Routing defaultly to EntityModelXHRResponder

--- responder/entity.model.xhr.routes.php
+++ responder/entity.model.xhr.routes.php
@@ -5,21 +5,21 @@ return array(
 		
 	'\/(.*)\/([0-9]+)(\?(.*)){0,1}' => 
 		array( 
-			'ApiResponder' , 
+			'EntityModelXHRResponder' , 
 			array( 'action' => 'findById' ) , 
 			array( 'entity' => 1 , 'id' => 2 ) 
 		) ,
 	
 	'\/(.*)\/@([A-Za-z_]+[A-Za-z0-9_]*)(\?(.*)){0,1}' => 
 		array( 
-			'ApiResponder' , 			
+			'EntityModelXHRResponder' , 			
 			array(),
 			array( 'entity' => 1 , 'action' => 2 ) 
 		) ,
 	
 	'\/(.*)\/(\?(.*)){0,1}' => 
 		array( 
-			'ApiResponder' , 
+			'EntityModelXHRResponder' , 
 			array( 'action' => 'find' ) , 
 			array( 'entity' => 1 ) 
 		) ,
