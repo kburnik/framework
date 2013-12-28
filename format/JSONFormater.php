b799b76c24ffd3c67d6fa204d3e6393e45dd2b9d
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-28 12:21:09 +0100

    Refactoring for readability

--- format/JSONFormater.php
+++ format/JSONFormater.php
@@ -1,21 +1,27 @@
 <?
 include_once(dirname(__FILE__).'/../base/Base.php');
 
-class JSONFormater implements IOutputFormater {
+class JSONFormater implements IOutputFormater 
+{
 	private $pretty;
 	
-	function __construct($pretty = false) {
+	function __construct($pretty = false)
+	{
 		$this->pretty = $pretty;
 	}
 
-	function Initialize() {	
+	function Initialize()
+	{	
 		header('Content-type: application/json');
 	}
 	
 	function Format($data) {
-		if (!$this->pretty) {
+		if (!$this->pretty) 
+		{
 			return json_encode($data);
-		} else {
+		} 
+		else 
+		{
 			return json_format(json_encode($data));
 		}
 	}
