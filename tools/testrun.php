4e078014270590ac1b179cfdeaa044d1a70efe4b
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-28 12:20:15 +0100

    testrun can now run without a test project

--- tools/testrun.php
+++ tools/testrun.php
@@ -1,8 +1,12 @@
 #!/usr/local/bin/php
 <?
-include_once( dirname(__FILE__) . '/../testproject/project.php' );
+include_once( dirname(__FILE__) . '/../base/Base.php' );
+@include_once( dirname(__FILE__) . '/../testproject/project.php' );
 
 array_shift( $argv );
+
+
+Console::Disable();
 
 TestUnitModule::run( $argv );
 
