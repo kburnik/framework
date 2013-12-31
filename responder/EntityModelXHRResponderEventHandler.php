90c296da8f0a827001fb3f3d8470ebff991b08d2
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-31 08:59:53 +0100

    Default handler for entity model responders

---
+++ responder/EntityModelXHRResponderEventHandler.php
@@ -0,0 +1,71 @@
+<?
+
+class EntityModelXHRResponderEventHandler implements IEntityModelXHRResponderEventHandler {
+
+	function onInsert( $responder , $entityModel , $data, $result )
+	{
+		$entityName = $entityModel->getEntityClassName();
+		
+		$responder->__setMessageView(
+			'successInsert',
+			array(
+				"entityName" => $entityName,
+				"id" => $result->id,
+			)			
+		);
+	}
+
+	function onUpdate( $responder , $entityModel , $data, $result )
+	{
+	
+		$entityName = $entityModel->getEntityClassName();
+		if ( $result > 0 )
+		{
+			$responder->__setMessageView(				
+				'sucessUpdateChanges',
+				array(
+					"entityName" => $entityName,
+					"id" => $data['id'],
+				)				
+			);		
+		} 
+		else 
+		{
+			$responder->__setMessageView(
+				'successUpdateNoChanges',
+				array()				
+			);
+		}
+	
+	}
+	
+	function onDelete( $responder , $entityModel , $data, $result )
+	{
+	
+		$entityName = $entityModel->getEntityClassName();
+		
+		if ( $result > 0 )
+		{
+			$responder->__setMessageView(				
+				'successDeleteChanges',
+				array(
+					"entityName" => $entityName,
+					"id" => $data['id'],
+				)				
+			);
+		}
+		else
+		{
+			$responder->__setMessageView(				
+				'successDeleteNoChanges',
+				array()
+			);
+		
+		}
+	
+	}
+
+
+}
+
+?>
