8fbe27cea714b56c5e794589e1131ce86aa97f5f
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-31 09:06:02 +0100

    EOL in unix format

--- entity/EntityModelFactory.php
+++ entity/EntityModelFactory.php
@@ -1,1 +1,25 @@
-<?class EntityModelFactory implements IEntityModelFactory {	public function createModelForEntity( $entityClassName , $dataDriver = null )	{			$entityModelClassName  = "{$entityClassName}Model";				if ( !class_exists( $entityModelClassName ) )		{					throw new Exception("No EntityModel found '{$entityModelClassName}'");				}				return new $entityModelClassName( $dataDriver );			}}?>
+<?
+
+class EntityModelFactory implements IEntityModelFactory 
+{
+
+	public function createModelForEntity( $entityClassName , $dataDriver = null )
+	{
+	
+		$entityModelClassName  = "{$entityClassName}Model";
+		
+		if ( !class_exists( $entityModelClassName ) )
+		{
+		
+			throw new Exception("No EntityModel found '{$entityModelClassName}'");
+		
+		}
+		
+		return new $entityModelClassName( $dataDriver );
+		
+	}
+
+
+}
+
+?>
