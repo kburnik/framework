19dc1befb746df0646033ce39788ebdd2db94a74
perica <peronja@gmail.com>
2014-06-16 13:20:55 +0200

    EntityBulk class

---
+++ entity/EntityBulk.php
@@ -0,0 +1,40 @@
+<?
+
+class EntityBulk
+{
+
+	private $items;
+	
+	public function __construct( $items )
+	{
+		
+		$this->items = $items;	
+	}
+	
+	public function __call( $method , $params )
+	{
+		
+		foreach ( $this->items as $item )
+		{
+			
+			if ( !method_exists( $item,$method ) )
+				throw new Exception("Missing method for bulk operation ".get_class($item)."::{{$method}}");
+			
+			call_user_func_array( array( $item , $method ) , $params  );
+		}
+	
+			
+		return $this;
+	
+	}
+	
+	
+	public function yield()
+	{
+		return $this->items;
+	}
+
+}
+
+
+?>
