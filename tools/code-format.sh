0bab76b0967bc935b84746232745f632efe33a29
Kristijan Burnik <burnik@chromium.org>
2014-11-05 14:46:35 +0100

    Update code formatting tool

--- tools/code-format.sh
+++ tools/code-format.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Temporary for all in/out.
+tmp=$(mktemp)
+
 # Replace tabs with double space.
 sed -i -e "s/\t/  /g" $@
 
-# Remove \r.
-sed -i -re "s/\r//g" $@
+# Replace \r with \n. \n\n will be replaced by cat.
+sed -i -re "s/\r/\n/g" $@
 
 # Remove spaces before and after parens.
 sed -i -re "s/\( */\(/g" $@
@@ -17,14 +20,15 @@ sed -i -re "s/, */,/g" $@
 # Add single space after comma.
 sed -i -re "s/,/, /g" $@
 
-# Put left brace inline.
-sed -i -re "N;/\n/s/\n *\{/ \{/g" $@
-
-# Remove all double newline occurences.
-tmp=$(mktemp)
 for x in $@; do
-  cp $x $tmp
-  cat -s $tmp > $x
+  # Remove all double newline occurences.
+  cp $x $tmp && cat -s $tmp > $x
+
+  # Put left brace inline.
+  number_of_occurrences=$(grep -o "\{" < $x | wc -l)
+  for y in $(seq 1 $number_of_occurrences); do
+    sed -i -re "N;/\n/s/\n *\{/ \{/g" $x
+  done;
 done;
 
 # Compact brackets.
@@ -37,3 +41,12 @@ sed -i -re "s/\! +/\!/g" $@
 # TODO: Make comments start with capital and end with .
 
 # TODO: Remove empty lines after return.
+
+# TODO: single spaces between concat . operator
+
+# TODO: else in format } else {
+
+# TODO: newline before break, continue, return
+
+# Clean up.
+rm $tmp
