ea1512fa77720939f6c824585601e6bf107c5a19
perica <peronja@gmail.com>
2015-01-05 03:53:32 +0100

    Use /usr/bin/env php in callscript for async calls.

--- async/callscript.sh
+++ async/callscript.sh
@@ -5,12 +5,12 @@
 touch $temp
 
 ########
-dt=$(date +"%Y-%m-%d %H:%M:%S") 
+dt=$(date +"%Y-%m-%d %H:%M:%S")
 nt=$(date +%s%N | cut -b1-13);
 echo "SH $$ $dt $nt :: AsyncStart - $2" >> $(dirname $0)/async.log.txt
 ########
 
-/usr/local/bin/php $1
+/usr/bin/env php $1
 
 ########
 dt=$(date +"%Y-%m-%d %H:%M:%S")
@@ -35,18 +35,18 @@ max_wait_time=600;
 
 wait_time=$max_wait_time;
 while [ -f "$temp" ] ; do
-	
+
 		########
-		dt=$(date +"%Y-%m-%d %H:%M:%S") 
+		dt=$(date +"%Y-%m-%d %H:%M:%S")
 		nt=$(date +%s%N | cut -b1-13);
 		echo "SH $$ $dt $nt :: Async waiting $wait_time ... " >> $(dirname $0)/async.log.txt
 		########
-		
+
 		sleep 1;
 		wait_time=$((wait_time-1));
 		if [  $wait_time -lt 0 ] ; then
 			########
-			dt=$(date +"%Y-%m-%d %H:%M:%S") 
+			dt=$(date +"%Y-%m-%d %H:%M:%S")
 			nt=$(date +%s%N | cut -b1-13);
 			echo "SH $$ $dt $nt :: Waited for $max_wait_time seconds and gave up" >> $(dirname $0)/async.log.txt
 			########
@@ -59,30 +59,30 @@ done;
 if [ -f "$temp" ] ;
 then
 	########
-	dt=$(date +"%Y-%m-%d %H:%M:%S") 
+	dt=$(date +"%Y-%m-%d %H:%M:%S")
 	nt=$(date +%s%N | cut -b1-13);
 	echo "SH $$ $dt $nt :: Cancelling since already running... " >> $(dirname $0)/async.log.txt
 	########
-	
+
 else
 	touch $temp
-	
+
 	########
-	dt=$(date +"%Y-%m-%d %H:%M:%S") 
+	dt=$(date +"%Y-%m-%d %H:%M:%S")
 	nt=$(date +%s%N | cut -b1-13);
 	echo "SH $$ $dt $nt :: Running async script $@" >> $(dirname $0)/async.log.txt
 	########
-	
-	/usr/local/bin/php $@
-	
+
+	/usr/bin/env php $@
+
 	########
 	dt=$(date +"%Y-%m-%d %H:%M:%S")
 	nt=$(date +%s%N | cut -b1-13);
 	echo "SH $$ $dt $nt :: Finished async script $@" >> $(dirname $0)/async.log.txt
 	########
-	
+
 	rm $temp
-fi; 
+fi;
 
 echo "" >> $out
-	
+
