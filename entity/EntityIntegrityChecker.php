600fc1e14dfc0d2f04f5ee22f39e08f9614d426d
Kristijan Burnik <burnik@chromium.org>
2015-01-12 13:31:42 +0100

    Basic integrity checking for entity.

---
+++ entity/EntityIntegrityChecker.php
@@ -0,0 +1,53 @@
+<?
+
+class IntegrityMessage {
+  private $data;
+
+  public function write($data) {
+    $this->data = $data;
+  }
+
+  public function read() {
+    return $this->data;
+  }
+}
+
+abstract class EntityIntegrityChecker {
+  protected $model;
+
+  public function __construct() {
+    $derivedClassName = get_class($this);
+    $modelClassName =
+        str_replace('IntegrityChecker', '', $derivedClassName) . "Model";
+    $this->model = $modelClassName::getInstance();
+
+  }
+
+  public function getIntegrityResults() {
+    $derivedClassName = get_class( $this );
+    $rc = new ReflectionClass( $derivedClassName );
+    $methods = $rc->getMethods(ReflectionMethod::IS_PUBLIC);
+
+    $results = array();
+
+    foreach ($methods as $method) {
+      if ($method->class != $derivedClassName)
+        continue;
+
+      $method_name = $method->name;
+
+      $message = new IntegrityMessage();
+      $result = $this->$method_name($message);
+
+      $results[] = array(
+        "class" => $method->class,
+        "method" => $method->name,
+        "result" => $result,
+        "message" => $message->read()
+      );
+    }
+    return $results;
+  }
+}
+
+?>
---
+++ tools/integrity-check.php
@@ -0,0 +1,51 @@
+#!/usr/bin/env php
+<?
+include_once(dirname(__FILE__) . "/../base/Base.php");
+
+class IntegrityCheckCrawler extends EntityCrawler {
+  protected function handleEntity($sourceEntry, $entityName) {
+    $integrityCheckerClassName = "{$entityName}IntegrityChecker";
+    if (class_exists($integrityCheckerClassName)) {
+      $checker = new $integrityCheckerClassName();
+
+      $checks = $checker->getIntegrityResults();
+
+      $sc = ShellColors::getInstance();
+      foreach ($checks as $check) {
+        $descriptor = $check['class'] . '::' . $check['method'];
+        echo $sc->getColoredString("$descriptor", "yellow") . " ";
+        if ($check['result']) {
+          echo $sc->getColoredString("OK", "green");
+        } else {
+          echo $sc->getColoredString("FAIL", "red") . "\n" ;
+          echo $check['message'];
+        }
+        echo "\n";
+      }
+
+    }
+  }
+
+  public function check($dir) {
+    $this->resolveProject($dir);
+    $this->traverse($dir);
+  }
+}
+
+$c = new IntegrityCheckCrawler();
+
+flush();
+ob_flush();
+ob_end_flush();
+
+$fs = new FileSystem();
+
+if (isset($argv[1]))
+  $dir = $fs->realpath(strtolower($argv[1]));
+else
+  $dir = $fs->getcwd();
+
+$c->check($dir);
+
+
+?>
