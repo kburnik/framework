8f63a01acbea56dca9610c9639aeba5d6d48b35f
Kristijan Burnik <kristijanburnik@gmail.com>
2015-03-27 20:49:36 +0100

    FileSystemSchedulerTest: Use another folder for each test fixture.

--- schedulable/testsuite/FileSystemSchedulerTestCase.php
+++ schedulable/testsuite/FileSystemSchedulerTestCase.php
@@ -1,10 +1,11 @@
-git config --global --edit<?php
+<?php
 
 class FileSystemSchedulerTestCase extends SchedulerTestCaseBase {
   public static $inherits = array('SchedulerTestCaseBase');
 
   public function __construct() {
-    $directory = dirname(__FILE__) . "/temp";
+    $date = intval(microtime(true) * 1000);
+    $directory = dirname(__FILE__) . "/temp/$date";
     $taskProvider = new ScheduledTaskProvider($directory);
     parent::__construct($taskProvider);
   }
