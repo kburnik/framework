e16b59cf78f6c1ff86b0f2abb49ab82edf61539e
Kristijan Burnik <kristijanburnik@gmail.com>
2015-11-23 23:05:03 +0100

    Make static method in Security.php. Clean up.

--- component/Security.php
+++ component/Security.php
@@ -1,53 +1,31 @@
-<?
+<?php
 
-class Security
-{
+class Security {
 
-
-  public static function GetRequestToken( $seconds = 10800 )
-  {
-
-    $storage = new SessionStorage( 'Security' );
-
-    $requestToken =  sha1( microtime( true ) . rand(11111,99999) );
-
-    $storage[ $requestToken ] = strtotime( now() ) + $seconds ;
+  public static function GetRequestToken($seconds = 10800) {
+    $storage = new SessionStorage('Security');
+    $requestToken = sha1(microtime(true) . rand(11111, 99999));
+    $storage[$requestToken] = strtotime(now()) + $seconds ;
 
     return $requestToken;
   }
 
+  public static function checkRequest($requestToken) {
+    $storage = new SessionStorage('Security');
 
-  public function checkRequest( $requestToken )
-  {
-
-    $storage = new SessionStorage( 'Security' );
-
-    if ( !isset( $requestToken ) )
-    {
-      throw new SecurityException("Security exception, no token provided!" , 1);
+    if (!isset($requestToken)) {
+      throw new SecurityException("Security exception: no token provided!", 1);
+    } else if (!isset($storage[$requestToken])) {
+      throw new SecurityException("Security exception: invalid token provided!",
+                                  2);
     }
-    else if ( !isset( $storage[ $requestToken ] ) )
-    {
-      throw new SecurityException("Security exception, invalid token provided!" ,2);
-    }
-    else if ( $storage[ $requestToken ] < strtotime( now() ) )
-    {
-      unset( $storage[ $requestToken ] );
-      throw new SecurityException("Security exception, token expired!",3);
-    }
-    else
-    {
+    else if ($storage[$requestToken] < strtotime(now())) {
+      unset($storage[$requestToken]);
+      throw new SecurityException("Security exception: token expired!", 3);
+    } else {
       // remove request token not to get clogged up
       unset( $storage[ $requestToken ] );
-
-      // all is ok
     }
-
   }
 
-
 }
-
-
-
-?>
