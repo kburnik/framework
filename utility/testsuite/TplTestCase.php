76e3ae73e781f69da6460c7f449e1d9750f6a7ad
Kristijan Burnik <kristijanburnik@gmail.com>
2016-01-03 00:08:06 +0100

    Enumerate tests.

--- utility/testsuite/TplTestCase.php
+++ utility/testsuite/TplTestCase.php
@@ -4,50 +4,56 @@ include_once( dirname(__FILE__) . "/../../base/Base.php" );
 
 class TplTestCase extends TestCase {
 
-  public function test0() {
+  public function test1() {
     $this->assertProduced("123",
                          '$([*]){[*]}',
                          array(1, 2, 3));
   }
 
-  public function test00() {
+  public function test2() {
     $this->assertProduced("112233",
                          '$([*]){[*][*]}',
                          array(1, 2, 3));
 
   }
 
-  public function test000() {
+  public function test3() {
     $this->assertProduced("1x12x23x3",
                          '$([*]){[*]x[*]}',
                          array(1, 2, 3));
   }
 
-  public function test0000() {
+  public function test4() {
     $this->assertProduced("123",
                          '${[*]}',
                          array(1, 2, 3));
   }
 
-  public function test1() {
+   public function test5() {
+    $this->assertProduced("Literal",
+                         'Literal',
+                         array(1, 2, 3));
+  }
+
+  public function test6() {
     $this->assertProduced("Hello1Hello2Hello3",
                          '$([*]){Hello[*]}',
                          array(1, 2, 3));
   }
 
-  public function test2() {
+  public function test7() {
     $this->assertProduced("1World2World3World",
                          '$([*]){[*]World}',
                          array(1, 2, 3));
   }
 
-  public function test3() {
+  public function test8() {
     $this->assertProduced("Hello1WorldHello2WorldHello3World",
                          '$([*]){Hello[*]World}',
                          array(1, 2, 3));
   }
 
-  public function test4() {
+  public function test9() {
      $this->assertProduced("123;456;789;",
                          '${${[*]};}',
                          array(array(1, 2, 3),
@@ -55,49 +61,49 @@ class TplTestCase extends TestCase {
                                array(7, 8, 9)));
   }
 
-  public function test5() {
+  public function test10() {
     $this->assertProduced("123",
                          '$([a]){[*]}',
                          array("a" => array(1, 2, 3)));
   }
 
-  public function test6() {
+  public function test11() {
     $this->assertProduced("111",
                          '$([a]){[**.0]}',
                          array("a" => array(1, 2, 3)));
   }
 
-  public function test7() {
+  public function test12() {
     $this->assertProduced("abc",
                          '${[#]}',
                          array("a" => 1, "b" => 2, "c" => 3));
   }
 
-  public function test8() {
+  public function test13() {
     $this->assertProduced("a->1; b->2; c->3; ",
                          '${[#]->[*]; }',
                          array("a" => 1, "b" => 2, "c" => 3));
   }
 
-  public function test9() {
+  public function test14() {
     $this->assertProduced("1 2 3",
                          '[a] [b] [c]',
                          array("a" => 1, "b" => 2, "c" => 3));
   }
 
-  public function test10() {
+  public function test15() {
     $this->assertProduced("1 2 3",
                          '${[a] [b] [c]}',
                          array(array("a" => 1, "b" => 2, "c" => 3)));
   }
 
-  public function test11() {
+  public function test16() {
     $this->assertProduced("1 2 3",
                          '$([*]){[a] [b] [c]}',
                          array(array("a" => 1, "b" => 2, "c" => 3)));
   }
 
-  public function test12() {
+  public function test17() {
     $data = array(
       "numbers" => array(1, 2, 3),
       "letters" => array("a", "b", "c")
@@ -107,7 +113,7 @@ class TplTestCase extends TestCase {
     $this->assertProduced("123abc", $template, $data);
   }
 
-  public function test13() {
+  public function test18() {
     $data = array(
       array(
         "numbers" => array(1, 2, 3),
@@ -120,7 +126,7 @@ class TplTestCase extends TestCase {
   }
 
 
-  public function test14() {
+  public function test19() {
     $data = array(
      array( "ID" => "1" , "name" => "Jimmy" , "surname" => "Hendrix" ),
      array( "ID" => "2" , "name" => "James" , "surname" => "Hetfield" ),
