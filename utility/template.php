a9b6d859f93e75de40edc803dfb1551ea476a26f
Kristijan Burnik <kristijanburnik@gmail.com>
2016-02-24 16:44:02 +0100

    Some versions of PHP don't work well with the -l option, so skip validation on templates by default.

--- utility/template.php
+++ utility/template.php
@@ -6,7 +6,7 @@ function produce($template,
                  $data = array(),
                  $use_cache = true,
                  $do_warn = true,
-                 $do_validate = true) {
+                 $do_validate = false) {
   $tpl = new Tpl(false);
 
   return $tpl->produce($template, $data, $use_cache, $do_warn, $do_validate);
@@ -16,7 +16,7 @@ function produceview($filename,
                      $data,
                      $use_cache = true,
                      $do_warn = true,
-                     $do_validate = true) {
+                     $do_validate = false) {
   return produce(get_once($filename),
                  $data,
                  $use_cache,
